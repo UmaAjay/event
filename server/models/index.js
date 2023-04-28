5e9b95c24e4275c88f0585ac46e743e16458636c
UMA AJAY KUMAR REDDY P S <umaveena.alvas@gmail.com>
2023-04-28 11:49:34 +0530

    update

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Event = require('./Event');
-const TokenBlacklist = require('./TokenBlacklist');
+const TokenBlacklist = require('./TokenBlackList');
 
 module.exports = {
     User,
